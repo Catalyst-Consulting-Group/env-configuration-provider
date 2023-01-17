9fb132d49f8035da858600dc3715756e268bedfb
Derek McKinnon <mckinnon.derek@gmail.com>
2023-01-17 15:32:11 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# CatConsult.EnvConfigurationProvider
+
+WIP
---
+++ test/CatConsult.EnvConfigurationProvider.Tests/UnitTest1.cs
@@ -0,0 +1,10 @@
+namespace CatConsult.EnvConfigurationProvider.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
